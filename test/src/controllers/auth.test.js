0ba62f9f6be7e83e84efe7075862aa193e22f932
Anoop Varghese <anupvarghese@gmail.com>
2017-05-14 18:51:45 +1000

    update test

--- test/src/controllers/auth.test.js
+++ test/src/controllers/auth.test.js
@@ -2,9 +2,9 @@ const { test } = require('ava');
 const sinon = require('sinon');
 const auth = require('../../../src/controllers/auth');
 
-test('Should respond', t => {
+test('Should respond', async t => {
   const req = { params: { name: 'hey' } };
   const rep = sinon.stub();
-  auth.login(req, rep);
+  await auth.login(req, rep);
   t.is(rep.callCount, 1);
 });
